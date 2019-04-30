6d0640ffe449d572b12a384492b8e16d103e6562
Macklin Thomas <Macklinthomas5@gmail.com>
2019-04-30 16:23:26 -0500

    Update and rename README.md to PythonHomework.py

---
+++ PythonHomework.py
@@ -0,0 +1,16 @@
+# TSU-Spring2019-CS456-Thomas2.0
+fileName = raw_input('Enter the file name you would like to read ') ("This line ask the user what file they would like to open.")
+try:
+    fileHandler = open(fileName)  ("This file opens the file that the user enters.")
+except:
+    print 'File cannot be opened:', fileName    ("If the files doesnt open then the message will pop up.")
+    exit()
+counts = dict()      ("This creates the dictionary for the the words.")
+for line in fileHandler:    ("This loops through the file and finds the words and keep count if them in the dictoinary.")
+    words = line.split()      ("This takes the word and stores it into a variable.")
+    for word in words:       ("If the word is in the dictionary or if it isnt")
+        if word not in counts:       ("Keep count of the added word.")
+            counts[word] = 1
+        else:
+            counts[word] += 1       ("Keep count of the word if its already stored.")
+print counts                           ("Prints the dictionary.")
